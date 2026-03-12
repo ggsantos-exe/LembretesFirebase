1bba24f0ce17dd3a161c5e6ce23f2d0ba4fe75a9
guigayzin <guilherme.gsantos02@gmail.com>
2026-03-12 16:14:29 -0300

    Cadastro de usuário implementado, correções em IDs e regForm

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,22 @@
-authForm.onsubmit = function(event){
 
-    event.preventDefault()
-    if (authForm.submitAuthForm.innerHTML == 'Entrar') {
-        firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).then(function (user) { 
-            
-            console.log('Acessou com sucesso =)')
-            console.log(user)
+authForm.onsubmit = function (event) {
+  event.preventDefault();
+  firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value)
+    .then(userCredential => {
+      console.log('Login ok', userCredential.user);
+    })
+    .catch(error => console.log('Erro no login', error));
+};
 
-        }).catch(function (error){
-            console.log('Falha no acesso =(')
-            console.log(error)
-
-        })
-
-    } else {
-
-    }
-
+regForm.onsubmit = function (event) {
+  event.preventDefault();
+  if (regForm.password.value === regForm.passwordCheck.value) {
+    firebase.auth().createUserWithEmailAndPassword(regForm.email.value, regForm.password.value)
+      .then(userCredential => {
+        console.log('Cadastro ok', userCredential.user);
+      })
+      .catch(error => console.log('Erro no cadastro', error));
+  } else {
+    console.log('Senhas não coincidem');
+  }
 }
